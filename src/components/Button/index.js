fd2829303c4ffa0ea9663ce79bbd7c4fe4136de2
EDEN <edenlesik@gmail.com>
2023-05-22 16:40:27 +0900

    feature header & header modal layout

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,7 +1,82 @@
-import React from "react";
+import styled from "styled-components";
+import {
+  main,
+  mainHover,
+  vividPink,
+  pinkHover,
+  purple,
+  yellow,
+  yellowHover,
+  orange,
+  orangeHover,
+  black,
+} from "@/styles/theme";
 
-const Button = () => {
-  return <div>버튼</div>;
+const Button = ({ icon, text, color, onClick, disabled, className }) => {
+  return (
+    <ButtonContainer
+      color={color}
+      onClick={onClick}
+      disabled={disabled}
+      className={className}
+    >
+      {icon}
+      {text}
+    </ButtonContainer>
+  );
 };
+
+const ButtonContainer = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  min-width: 150px;
+  padding: 15px 20px;
+  font-size: 1rem;
+  font-size: ${(props) => props.color === "transparent" && "0.75rem"};
+  font-weight: 700;
+  line-height: 1.5;
+  background: ${(props) => props.color === "main" && `${main}`};
+  background: ${(props) => props.color === "purple" && "transparent"};
+  background: ${(props) => props.color === "yellow" && `${yellow}`};
+  background: ${(props) => props.color === "pink" && `${vividPink}`};
+  background: ${(props) => props.color === "orange" && `${orange}`};
+  background: ${(props) => props.color === "transparent" && "transparent"};
+  border: ${(props) => props.color === "purple" && `1px solid ${purple}`};
+  border-radius: 54px;
+  transition: all ease 0.2s;
+
+  &:hover {
+    background: ${(props) => props.color === "main" && `${mainHover}`};
+    background: ${(props) => props.color === "purple" && `${purple}`};
+    background: ${(props) => props.color === "yellow" && `${yellowHover}`};
+    background: ${(props) => props.color === "pink" && `${pinkHover}`};
+    background: ${(props) => props.color === "orange" && `${orangeHover}`};
+    color: ${(props) => props.color === "transparent" && "rgba(0,0,0,0.7)"};
+    cursor: pointer;
+  }
+
+  &:disabled {
+    background: ${(props) => props.color === "main" && `${main}`};
+    background: ${(props) => props.color === "purple" && "transparent"};
+    background: ${(props) => props.color === "yellow" && `${yellow}`};
+    background: ${(props) => props.color === "pink" && `${vividPink}`};
+    background: ${(props) => props.color === "orange" && `${orange}`};
+    border: ${(props) => props.color === "purple" && `1px solid ${purple}`};
+    color: ${black};
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
+
+  @media screen and (max-width: 939px) and (min-width: 767px),
+    screen and (max-width: 766px) {
+    min-width: 100px;
+    padding: 15px 21px;
+    font-size: 0.9rem;
+    font-size: ${(props) => props.color === "transparent" && "0.5rem"};
+    line-height: 1;
+  }
+`;
 
 export default Button;
