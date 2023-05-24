599c85617a0eadf8eed0ca1cea2be29dd2297c46
EDEN <edenlesik@gmail.com>
2023-05-24 15:22:16 +0900

    feature customer & introduce lesik

--- src/pages/customer/index.js
+++ src/pages/customer/index.js
@@ -1,7 +1,33 @@
-import React from "react";
+import { useRouter } from "next/router";
+import styled from "styled-components";
+import MainContent from "@/components/MainContent";
+import SubContent from "@/components/SubContent";
+import food from "@/assets/makeat2.jpg";
 
 const Customer = () => {
-  return <div>고객</div>;
+  const router = useRouter();
+  const navigateToMakeatPage = () => router.push("/makeat");
+
+  return (
+    <CustomerContainer>
+      <MainContent
+        image={food}
+        headline={
+          <>
+            This is
+            <br /> makeat
+          </>
+        }
+        text="makeat 다운로드"
+        onClick={navigateToMakeatPage}
+        color="pink"
+      />
+      <SubContent label="you all makeat" headline="ddd" />
+      <SubContent color="on" label="you all makeat" headline="ddd" />
+    </CustomerContainer>
+  );
 };
+
+const CustomerContainer = styled.main``;
 
 export default Customer;
