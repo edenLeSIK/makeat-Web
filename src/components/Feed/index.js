bf22020dca8b1a94a28b13b000a81edcfce6a872
EDEN <edenlesik@gmail.com>
2023-05-25 12:52:27 +0900

    feature introduce lesik

--- src/components/Feed/index.js
+++ src/components/Feed/index.js
@@ -10,8 +10,10 @@ const Feed = () => {
     <FeedContainer>
       <div className="headline-wrapper">
         <h6>latest news</h6>
-        <h2>lates dd readread make read</h2>
-        <Button text="Read More" color="yellow" />
+        <h2>We are all</h2>
+        <div className="hide-on-mobile">
+          <Button text="Read More" color="yellow" />
+        </div>
       </div>
       <div className="feed-wrapper">
         {a.map((_, i) => (
@@ -35,6 +37,20 @@ const FeedContainer = styled.section`
   padding-right: 4.44vw;
   background: #20202b;
 
+  @media screen and (max-width: 939px) and (min-width: 767px),
+    screen and (max-width: 766px) {
+    display: -webkit- flex;
+    display: flex;
+    -webkit-flex-direction: column;
+    flex-direction: column;
+    -webkit-align-items: center;
+    align-items: center;
+    -webkit-justify-content: stretch;
+    justify-content: stretch;
+    row-gap: 40px;
+    padding: 64px 4.44vw;
+  }
+
   .headline-wrapper {
     display: -webkit- flex;
     display: flex;
@@ -48,6 +64,12 @@ const FeedContainer = styled.section`
     width: 28.8933333333vw;
     padding: 0 calc(5.5583333333vw / 2) 0 0;
 
+    @media screen and (max-width: 939px) and (min-width: 767px),
+      screen and (max-width: 766px) {
+      width: 100%;
+      padding: 0;
+    }
+
     h6 {
       color: #9797a1;
       font-size: 0.8888888889rem;
@@ -55,6 +77,12 @@ const FeedContainer = styled.section`
       line-height: 1.1;
       letter-spacing: 0.1rem;
       text-transform: uppercase;
+
+      @media screen and (max-width: 939px) and (min-width: 767px),
+        screen and (max-width: 766px) {
+        font-size: 0.6666666667rem;
+        line-height: 1;
+      }
     }
 
     h2 {
@@ -64,11 +92,21 @@ const FeedContainer = styled.section`
       line-height: 0.95;
       letter-spacing: 0.01rem;
       text-transform: uppercase;
+
+      @media screen and (max-width: 939px) and (min-width: 767px),
+        screen and (max-width: 766px) {
+        font-size: 2.6666666667rem;
+      }
     }
   }
 
-  .content-wrapper {
+  .feed-wrapper {
     width: 60.0066666667vw;
+
+    @media screen and (max-width: 939px) and (min-width: 767px),
+      screen and (max-width: 766px) {
+      width: 100%;
+    }
   }
 `;
 
