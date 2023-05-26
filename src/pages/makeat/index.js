539b209bc9e0834a02abe382ca286405b24a2b0a
EDEN <edenlesik@gmail.com>
2023-05-26 13:41:04 +0900

    style makeat responsive

--- src/pages/makeat/index.js
+++ src/pages/makeat/index.js
@@ -5,12 +5,25 @@ import MakeatMobile from "@/components/MakeatMobile";
 const Makeat = () => {
   return (
     <MakeatContainer>
-      <MakeatPc />
-      <MakeatMobile />
+      <MakeatPc className="web" />
+      <MakeatMobile className="app" />
     </MakeatContainer>
   );
 };
 
-const MakeatContainer = styled.main``;
+const MakeatContainer = styled.main`
+  /* .web {
+    @media (max-width: 915px) {
+      display: none;
+    }
+  } */
+
+  /* .app {
+    @media (min-width: 475px) {
+      left: 50%;
+      margin-left: -237.5px;
+    }
+  } */
+`;
 
 export default Makeat;
