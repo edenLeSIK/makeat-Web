7b84d29950ec5b0f56920144114a2f4c019a3db8
EDEN <edenlesik@gmail.com>
2023-05-28 16:49:33 +0900

    fix news components conflict

--- src/components/News/index.js
+++ src/components/News/index.js
@@ -10,15 +10,7 @@ const News = () => {
           href="https://kr.aving.net/news/articleView.html?idxno=1773665"
           target="_blank"
         >
-<<<<<<< HEAD
-<<<<<<< HEAD
           <h6 className="date">23/05/28</h6>
-=======
-          <h6 className="date">2023/05/28</h6>
->>>>>>> main
-=======
-          <h6 className="date">23/05/28</h6>
->>>>>>> main
           <div className="contents">
             <h5 className="title">
               제목이 제목 제목 제목이제 목 제목이제목 제
@@ -60,21 +52,12 @@ const NewsContainer = styled.article`
     grid-template: auto auto/1fr auto 1fr;
     padding: 32px 0;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> main
     @media screen and (max-width: 939px) and (min-width: 767px),
       screen and (max-width: 766px) {
       grid-template: auto auto/auto auto 1fr;
       padding: 16px 0;
     }
 
-<<<<<<< HEAD
-=======
->>>>>>> main
-=======
->>>>>>> main
     .date {
       margin: 0 8px 0 0;
       align-self: start;
@@ -82,8 +65,6 @@ const NewsContainer = styled.article`
       color: ${gray};
       font-size: 0.8888888889rem;
       font-weight: 400;
-<<<<<<< HEAD
-<<<<<<< HEAD
       line-height: 1.5 !important;
       letter-spacing: 0.1rem;
       text-transform: uppercase;
@@ -93,51 +74,28 @@ const NewsContainer = styled.article`
         width: 100%;
         font-size: 0.6666666667rem;
       }
-=======
-      line-height: 1.1;
-      letter-spacing: 0.1rem;
-      text-transform: uppercase;
->>>>>>> main
-=======
-      line-height: 1.5 !important;
-      letter-spacing: 0.1rem;
-      text-transform: uppercase;
 
       @media screen and (max-width: 939px) and (min-width: 767px),
         screen and (max-width: 766px) {
         width: 100%;
         font-size: 0.6666666667rem;
       }
->>>>>>> main
     }
 
     .contents {
       width: 44.45vw;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> main
       @media screen and (max-width: 939px) and (min-width: 767px),
         screen and (max-width: 766px) {
         width: 62.2266666667vw;
       }
 
-<<<<<<< HEAD
-=======
->>>>>>> main
-=======
->>>>>>> main
       .title {
         color: ${white};
         font-size: 1.3333333333rem;
         font-weight: 400;
         line-height: 1.3;
         letter-spacing: -0.01rem;
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> main
         display: -webkit-box;
         -webkit-line-clamp: 1; /* 최대 표시할 줄 수 */
         -webkit-box-orient: vertical;
@@ -149,7 +107,6 @@ const NewsContainer = styled.article`
           font-size: 1rem;
           line-height: 1.25;
         }
-<<<<<<< HEAD
       }
 
       .info {
@@ -168,83 +125,40 @@ const NewsContainer = styled.article`
           line-height: 1.5;
           -webkit-line-clamp: 2;
         }
-=======
-=======
->>>>>>> main
       }
 
-      .info {
-        display: -webkit-box;
-        -webkit-line-clamp: 3;
-        -webkit-box-orient: vertical;
-        margin: 24px 0 0;
-        color: ${gray};
-<<<<<<< HEAD
->>>>>>> main
-=======
-        overflow: hidden;
-        text-overflow: ellipsis;
-        line-height: 1.25;
+      .magazine {
+        margin-bottom: 24px;
+        font-size: 1rem;
+        font-weight: 700;
+        line-height: 1.35;
+        letter-spacing: 0;
 
         @media screen and (max-width: 939px) and (min-width: 767px),
           screen and (max-width: 766px) {
+          font-weight: 400;
           font-size: 0.6666666667rem;
           line-height: 1.5;
-          -webkit-line-clamp: 2;
         }
->>>>>>> main
+      }
 
-        .magazine {
-          margin-bottom: 24px;
-          font-size: 1rem;
-          font-weight: 700;
-          line-height: 1.35;
-          letter-spacing: 0;
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> main
-
-          @media screen and (max-width: 939px) and (min-width: 767px),
-            screen and (max-width: 766px) {
-            font-weight: 400;
-            font-size: 0.6666666667rem;
-            line-height: 1.5;
-          }
-        }
-
-        .summary {
-<<<<<<< HEAD
-=======
->>>>>>> main
-=======
->>>>>>> main
-        }
+      .summary {
       }
     }
+  }
 
-    .icon {
-      width: 50px;
-      height: 50px;
-      justify-self: end;
-      align-self: start;
-      margin-right: 8px;
-      fill: #4a4a5a;
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> main
+  .icon {
+    width: 50px;
+    height: 50px;
+    justify-self: end;
+    align-self: start;
+    margin-right: 8px;
+    fill: #4a4a5a;
 
-      @media screen and (max-width: 939px) and (min-width: 767px),
-        screen and (max-width: 766px) {
-        width: 25px;
-        height: 25px;
-      }
-<<<<<<< HEAD
-=======
->>>>>>> main
-=======
->>>>>>> main
+    @media screen and (max-width: 939px) and (min-width: 767px),
+      screen and (max-width: 766px) {
+      width: 25px;
+      height: 25px;
     }
   }
 `;
